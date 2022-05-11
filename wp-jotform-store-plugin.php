6d501611ae0caf45d44464756d201c0e6651cdc6
omerkaplanjf <94830538+omerkaplanjf@users.noreply.github.com>
2022-05-11 10:46:28 +0300

    Update plugin author

--- wp-jotform-store-plugin.php
+++ wp-jotform-store-plugin.php
@@ -5,7 +5,8 @@
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Jotform.com
+ * Author URI:        https://www.jotform.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-jotform-store-plugin
