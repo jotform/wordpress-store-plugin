a8b821c4e9c13d73d2cab3df3bfc603633fcb3ac
Derya Yeşilyurt <derya@jotform.com>
2022-05-11 17:52:12 +0300

    Move static block config back to block.json

--- src/index.js
+++ src/index.js
@@ -5,37 +5,7 @@ import PublishMode from './publish/PublishMode';
 import { jotformIcon } from './assets/Icons';
 
 registerBlockType('create-block/wp-jotform-store-plugin', {
-	title: 'Jotform Store Embed',
 	icon: jotformIcon,
-	category: 'embed',
-	description: 'Create your online store and embed to your website in minutes.',
-	attributes: {
-		align: {
-			type: 'string',
-			default: 'wide'
-		},
-		blockPreview: {
-			type: 'boolean'
-		},
-		storeIcon: {
-			type: 'string'
-		},
-		storeId: {
-			type: 'string'
-		},
-		storeTitle: {
-			type: 'string'
-		}
-	},
-	example: {
-		attributes: {
-			blockPreview: true
-		}
-	},
-	supports: {
-		align: true,
-		html: false
-	},
 	edit: EditMode,
 	save: PublishMode,
 });
