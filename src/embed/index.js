c1e22cdd84ab6ccfde4901959929f57d3a2503d3
Derya Yeşilyurt <derya@jotform.com>
2022-06-07 12:16:59 +0300

    Add interactive overlay in edit mode to see the settings toolbar

--- src/embed/index.js
+++ src/embed/index.js
@@ -1,12 +1,17 @@
-const StoreEmbed = ({ attributes }) => {
+const StoreEmbed = ({ attributes, forEdit }) => {
 	const { storeId, storeTitle, height } = attributes;
+
 	return (
-		<iframe
-			id={`JotFormIFrame-${storeId}`}
-			title={storeTitle}
-			src={`https://www.jotform.com/app/${storeId}?appEmbedded=1`}
-			style={{ width: '100%', height }}
-		/>
+		<div className="wp-block-embed__wrapper" style={{ height }}>
+			<iframe
+				id={`JotFormIFrame-${storeId}`}
+				className="jf-app-frame"
+				title={storeTitle}
+				src={`https://www.jotform.com/app/${storeId}?appEmbedded=1`}
+				style={{ width: '100%', height: '100%' }}
+			/>
+			{forEdit && <div className="block-library-embed__interactive-overlay" />}
+		</div>
 	);
 };
 
