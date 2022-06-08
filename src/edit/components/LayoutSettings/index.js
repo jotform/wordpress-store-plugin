7d2db1a53f2ea2d359af30b36694b5c34b61c4e2
omerkaplanjf <94830538+omerkaplanjf@users.noreply.github.com>
2022-06-08 11:50:48 +0300

    Add width and align setting to layout settings

--- src/edit/components/LayoutSettings/index.js
+++ src/edit/components/LayoutSettings/index.js
@@ -1,18 +1,56 @@
 import {
+	SelectControl,
 	PanelBody,
 	__experimentalUnitControl as UnitControl, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 const LayoutSettings = ({ attributes, setAttributes }) => {
-	const { height } = attributes;
+	const { height, align } = attributes;
 
 	const handleHeightChange = (newHeight) => {
 		setAttributes({ height: newHeight });
 	};
 
+	const handleAlignChange = (newAlign) => {
+		setAttributes({ align: newAlign });
+	};
+
 	return (
 		<PanelBody title={__('Layout Settings')}>
+			<>
+				<SelectControl
+					label="Width &#38; Aligment"
+					value={align}
+					onChange={handleAlignChange}
+					options={[
+						{
+							label: 'None (max 650px)',
+							value: 'center',
+						},
+						{
+							label: 'Wide width (max 1000px)',
+							value: 'wide',
+						},
+						{
+							label: 'Wide width',
+							value: 'full',
+						},
+						{
+							label: 'Align right',
+							value: 'right',
+						},
+						{
+							label: 'Align left',
+							value: 'left',
+						},
+						{
+							label: 'Align center',
+							value: 'center',
+						},
+					]}
+				/>
+			</>
 			<UnitControl
 				label={__('Height')}
 				value={height}
