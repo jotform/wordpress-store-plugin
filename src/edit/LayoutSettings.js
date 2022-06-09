dbab0ca4994f5ac1dc11f3aec48679accb54838c
Derya Yeşilyurt <derya@jotform.com>
2022-06-09 16:20:05 +0300

    Update width and alignment settings styles

--- src/edit/LayoutSettings.js
+++ src/edit/LayoutSettings.js
@@ -1,53 +1,63 @@
 import {
-	SelectControl,
+	CustomSelectControl,
 	PanelBody,
 	__experimentalUnitControl as UnitControl, // eslint-disable-line @wordpress/no-unsafe-wp-apis
 } from '@wordpress/components';
 
+const alignOptions = [
+	{
+		name: 'None (max 650px wide)',
+		key: 'none',
+		className: 'dashicons-before dashicons-align-none',
+	},
+	{
+		name: 'Wide width (max 1000px wide)',
+		key: 'wide',
+		className: 'dashicons-before dashicons-align-wide',
+	},
+	{
+		name: 'Full width',
+		key: 'full',
+		className: 'dashicons-before dashicons-align-full-width',
+	},
+	{
+		name: 'Align left',
+		key: 'left',
+		className: 'dashicons-before dashicons-align-left',
+	},
+	{
+		name: 'Align center',
+		key: 'center',
+		className: 'dashicons-before dashicons-align-center',
+	},
+	{
+		name: 'Align right',
+		key: 'right',
+		className: 'dashicons-before dashicons-align-right',
+	},
+];
+
 const LayoutSettings = ({ attributes, setAttributes }) => {
 	const { height, align } = attributes;
-	const alignOptions = [
-		{
-			label: 'None (max 650px)',
-			value: 'center',
-		},
-		{
-			label: 'Wide width (max 1000px)',
-			value: 'wide',
-		},
-		{
-			label: 'Wide width',
-			value: 'full',
-		},
-		{
-			label: 'Align right',
-			value: 'right',
-		},
-		{
-			label: 'Align left',
-			value: 'left',
-		},
-		{
-			label: 'Align center',
-			value: 'center',
-		},
-	];
 
 	const handleHeightChange = (newHeight) => {
 		setAttributes({ height: newHeight });
 	};
 
-	const handleAlignChange = (newAlign) => {
-		setAttributes({ align: newAlign });
+	const handleAlignChange = ({ selectedItem }) => {
+		setAttributes({ align: selectedItem.key });
 	};
+
+	const selectedOption = alignOptions.find((option) => option.key === align);
 
 	return (
 		<PanelBody title="Layout Settings">
-			<SelectControl
-				label="Width &#38; Aligment"
-				value={align}
+			<CustomSelectControl
+				label="Width & Alignment"
+				value={selectedOption}
 				onChange={handleAlignChange}
 				options={alignOptions}
+				className="jf-custom-select-control"
 			/>
 			<UnitControl
 				label="Height"
