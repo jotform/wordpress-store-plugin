7acbe2ab1765f4b41b50e36d74d52ecf07c5f890
Derya Yeşilyurt <derya@jotform.com>
2022-06-16 12:10:58 +0300

    Add "noAuth" param to open the app in guest mode in preview

--- src/embed/index.js
+++ src/embed/index.js
@@ -1,7 +1,9 @@
 const StoreEmbed = ({ attributes, forEdit }) => {
 	const { storeId, storeTitle, height, headerVisibility, productListLayout } =
 		attributes;
-	const openAppHeader = headerVisibility ? 'Yes' : 'No';
+	const openAppHeader = headerVisibility ? '1' : '0';
+	let frameSrc = `https://www.jotform.com/app/${storeId}?appEmbedded=1&openAppHeader=${openAppHeader}&_productListLayout=${productListLayout}`;
+	if (forEdit) frameSrc = `${frameSrc}&noAuth=1`;
 
 	return (
 		<div className="wp-block-embed__wrapper" style={{ height }}>
@@ -9,7 +11,7 @@ const StoreEmbed = ({ attributes, forEdit }) => {
 				id={`JotFormIFrame-${storeId}`}
 				className="jf-app-frame"
 				title={storeTitle}
-				src={`https://www.jotform.com/app/${storeId}?appEmbedded=1&openAppHeader=${openAppHeader}&_productListLayout=${productListLayout}`}
+				src={frameSrc}
 				style={{ width: '100%', height: '100%' }}
 			/>
 			{forEdit && (
