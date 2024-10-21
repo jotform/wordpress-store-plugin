482a125b251e9a1bdd86adf711a32c0373f467f3
deryajotform <37997502+deryajotform@users.noreply.github.com>
2024-10-21 13:19:39 +0300

    Version 2.1.1: new release

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => 'c1f29fff3560e928875e');
